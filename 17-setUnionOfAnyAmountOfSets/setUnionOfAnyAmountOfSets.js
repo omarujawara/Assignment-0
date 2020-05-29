309bdea44c6dd7edf9628e8454c259444e73da60
Omaru Jawara <omaru.jawara94@gmail.com>
2020-05-29 00:01:21 -0400

    After code passing test

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,16 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
-}
+  let union = new Set();
+  args.forEach((setElement) => {
+    for (const elementOfSetElement of setElement) {
+      union.add(elementOfSetElement);
+    }
+  });
 
+  return union;
+}
+setUnionOfAnyAmountOfSets(
+  setUnionOfAnyAmountOfSets([1, 2, 4], [4, 5, 6], [6, 7, 8])
+);
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
