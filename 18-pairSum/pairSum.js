4102009beffee18670c15eb3bcc9046dc44e5b81
Omaru Jawara <omaru.jawara94@gmail.com>
2020-05-29 00:48:21 -0400

    Code testing and fails one test

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,22 @@
 function pairSum(nums, target) {
   // Insert code here;
+  try {
+    if (nums.length <= 1) throw Error;
+    let isSum = false;
+    for (let i = 0; i < nums.length; ++i) {
+      for (let j = 0; j < nums.length; ++j) {
+        if (nums[i] + nums[j] === target) {
+          isSum = true;
+          break;
+        }
+      }
+    }
+    return isSum;
+  } catch (error) {
+    return error;
+  }
 }
 
+// console.log(pairSum([0, 1, 2, 3, 4, 5], 8));
 // Do not edit this line;
 module.exports = pairSum;
