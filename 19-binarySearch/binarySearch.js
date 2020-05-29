58d64739a7ff3e417d00d209d53486fa5e307569
Omaru Jawara <omaru.jawara94@gmail.com>
2020-05-29 01:10:49 -0400

    Fails two tests

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,18 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let first = 0;
+    let last = nums.size - 1;
+    // let middle;
+    // let found = false;
+    // let position = false;
+    while (first <= last) {
+      let middle = Math.floor((first + last) / 2);
+      if (nums[middle] === target) return true;
+      else if (nums[middle] > target) last = middle - 1;
+      else first = middle + 1;
+    }
+    return false;
   }
 }
 
